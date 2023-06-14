58f8974bf98a7783ed0c3a244ce0df7aaf4240cc
Christopher Hill <cjameshill@gmail.com>
2023-06-14 18:24:52 +0200

    Added publish method to publish to keue

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
 import test, { ExecutionContext } from "ava";
-import HttpRequestWrapper from "./index";
+import KeueIO from "./index";
 
 test("GET request should return data", async (t: ExecutionContext) => {
-    const request = new HttpRequestWrapper();
+    const request = new KeueIO({ apiKey: "123", app: "test" });
     const response: any = await request.get(
         "https://webhook.site/0db2a3e4-37a8-4390-9e62-ef35ea8d3f86",
     );
@@ -10,11 +10,18 @@ test("GET request should return data", async (t: ExecutionContext) => {
 });
 
 test("POST request should return ID", async (t: ExecutionContext) => {
-    const request = new HttpRequestWrapper();
+    const request = new KeueIO({ apiKey: "123", app: "test" });
     const postData = { name: "John", age: 25 };
     const response: any = await request.post(
         "https://webhook.site/0db2a3e4-37a8-4390-9e62-ef35ea8d3f86",
         postData,
     );
     t.is(response.message, "world");
+});
+test("publish message", async (t: ExecutionContext) => {
+    const keue = new KeueIO({ apiKey: "123", app: "chris" });
+    const postData = { name: "John", age: 25 };
+    const response: any = await keue.publish(postData);
+    console.log("publish message", response);
+    t.truthy(response.messageId);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from "axios";
 
-class HttpRequestWrapper {
+interface KeueIOConfig {
+    apiKey: string;
+    app: string;
+}
+class KeueIO {
+    private instance: AxiosInstance;
+    constructor(protected config: KeueIOConfig) {
+        this.instance = axios.create({
+            baseURL: `https://${this.config.app}.beta.eu.keue.io`,
+        });
+        this.instance.interceptors.request.use((config) => {
+            config.params = {
+                ...config.params,
+                apiKey: this.config.apiKey,
+            };
+            config.headers["User-Agent"] = `KeueIO/${
+                process.env.npm_package_version || "latest"
+            }`;
+            return config;
+        });
+    }
     public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
-        const response: AxiosResponse<T> = await axios.get(url, config);
+        const response: AxiosResponse<T> = await this.instance.get(url, config);
         return response.data;
     }
 
@@ -11,11 +31,26 @@ class HttpRequestWrapper {
         data?: any,
         config?: AxiosRequestConfig,
     ): Promise<T> {
-        const response: AxiosResponse<T> = await axios.post(url, data, config);
+        const response: AxiosResponse<T> = await this.instance.post(
+            url,
+            data,
+            config,
+        );
         return response.data;
     }
-
+    public async publish<T>(
+        data: any = {},
+        tag: string = "default",
+    ): Promise<T> {
+        if (!this.config.apiKey) throw new Error("API Key is required");
+        if (!this.config.app) throw new Error("App is required");
+        const response: AxiosResponse<T> = await this.instance.post(
+            `/${tag}`,
+            data,
+        );
+        return response.data;
+    }
     // Add other HTTP methods as needed (e.g., put, delete, etc.)
 }
 
-export default HttpRequestWrapper;
+export default KeueIO;
